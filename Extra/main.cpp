9afbaf94ef8bde6c94c5e1c664a8e06badf8d765
mrvalentyn <95167780+mrvalentyn@users.noreply.github.com>
2023-11-14 21:30:34 +0200

    Add macro

---
+++ Extra/main.cpp
@@ -0,0 +1,16 @@
+/*All my macros placed inside a .cpp file,so I won't waste any time by writing all of this - mrvalentyn*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define intmx INT_MAX
+#define in ifstream f
+#define out ofstream g
+#define close f.close(); g.close();
+
+
+int main(){
+    return 0;
+
+}
