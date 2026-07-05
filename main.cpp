94f635a200e9045b604479b6cb658b602e01e373
xqvim <95167780+qvalentln@users.noreply.github.com>
2026-07-05 17:57:53 +0300

    Clean up main.cpp by removing comments and code
    Removed commented-out code and unnecessary comments.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
     *
     * soon on twitch : ex3qute 
     * 
-    * Everything works out in the end *
-    * It's you, it'll always be you *
-    * 
+    * ax ah al
+    * kwxkwxkxkxwkxkwxkw
+    * dumnezeu sa o ierte
 */
 
 
@@ -18,49 +18,12 @@ const int MOD = 1e9+7;
 int di[4]={0,0,-1,1};  
 int dj[4]={-1,1,0,0};  
 
-/*
-#ifndef exe
-#define cin f
-#define cout g
-#endif
-*/
-string FILENAME="special";  
-ifstream f(FILENAME+".in");  
-ofstream g(FILENAME+".out");  
-
-
 signed main(){  
-    
 
+    fastio
+
+    
     return 0;
 }
-/**
- * qué lástima,
- * qué lástima...
- * mírate, todo el día mirando el teléfono
- * a ver si HSEXHSAXH te escribe
- * HSEXHSAXH se fue...
- * no va a volver,
- * no te va a escribir
- * ahora está con otro,
- * pensando en el perro con otro,
- * pensando en el viaje con otro,
- * pensando en mejorar con otro,
- * riendo con otro,
- * y mírate, estás en el piso llorando...
- * acostado, triste...
- * rezando para que vuelva,
- * pidiéndole a Dios que vuelva...
- * no va a volver...
- * ¡NO VA A VOLVER!
- * lo que tiene que volver es TU MEJOR VERSIÓN
- * ESA VERSIÓN que es UNA BESTIA que todos los días va a correr,
- * que TODOS LOS DÍAS va a ENTRENAR,
- * cumple, y se siente orgullosa de sí misma,
- * así que ¡LEVÁNTATE!
- * y date cuenta de que NO VA A VOLVER
- * que el que va a volver
- * sos VOS,
- * el cambio SOY YO y depende de vos...
- * ¡ARRIBA!
- */
+
+
